9cd909bc40ecab214c0865a193af251b5c0e39a4
Sean Thames <thames.sean@gmail.com>
2019-04-25 10:04:11 -0400

    Interactive mode is completed. Next scripted.

--- Program.cs
+++ Program.cs
@@ -1,32 +1,47 @@
 ﻿using System;
+using System.IO;
 using System.IO.Compression;
 
 namespace core_zip
 {
     class Program
     {
-        static void Zipper(string folder, string file, bool overwrite)
+        static void Zipper(string folder, string file)
         {
-            ZipFile.CreateFromDirectory(@".\" + folder, @".\" + file);
+            ZipFile.CreateFromDirectory(folder, file);
         }
         static void Main(string[] args)
         {
             string inFolder2zip = "";
             string inZipFilename = "";
             string inOverwrite = "";
-            bool overwrite = false;
 
             if (args.Length == 0)
             {
-                Console.WriteLine("No args detected");
                 Console.WriteLine("What folder would you like to zip? ");
-                folder2zip = Console.ReadLine();
+                inFolder2zip = Console.ReadLine();
                 Console.WriteLine("What would you like to Call the zip?");
-                zipFilename = Console.ReadLine();
-                Console.WriteLine("Overwrite any existing zip? [y/N]");
-                inOverwrite = Console.ReadLine();
+                inZipFilename = Console.ReadLine();
 
-                Zipper(inFolder2zip, inZipFilename, overwrite);
+                if (File.Exists(inZipFilename))
+                {
+                    Console.WriteLine("Would you like to overwrite the existing file? [y/N]");
+                    inOverwrite = Console.ReadLine();
+                    
+                    if (inOverwrite == "y" || inOverwrite == "Y")
+                    {
+                        File.Delete(inZipFilename);
+                        Zipper(inFolder2zip, inZipFilename);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Aborting...");
+                    }
+                }
+                else
+                {
+                    Zipper(inFolder2zip, inZipFilename);
+                }
             }
             else
             {
